5f85161ca1fd2c27dc8db4903813ed3aef044bfb
Lavanya GT <lavanyagt3@gmail.com>
2024-12-11 22:10:29 +0530

    javav

---
+++ WhileDemo.java
@@ -0,0 +1,14 @@
+package BasicsOfJava;
+
+public class WhileDemo {
+	public static void main(String[] args) {
+		int i=1;
+		while(i<=10) //1<10
+		{
+			System.out.println("Tomorrow is holiday  "+i);
+			i++;
+		}
+
+	}
+
+}
